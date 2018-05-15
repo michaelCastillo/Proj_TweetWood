bcf0192b4dc3db182d97c8ffee91b99572b97725
Oscar <oscar.utreras@usach.cl>
2018-05-14 23:42:36 -0400

    Use of api with a query

--- Front/front_tweetwood/src/Film.vue
+++ Front/front_tweetwood/src/Film.vue
@@ -1,13 +1,31 @@
 <template>
-  <h1>{{msg}}</h1>
+  <div>
+    <div v-if="film != null">
+      <h1>{{film.original_title}}</h1>
+      <p>{{film.overview}}</p>
+    </div>
+    <span v-else>Loading...</span>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'film',
+  mounted(){
+    this.getFilm();
+  },
   data () {
     return {
-      msg: 'Películas'
+      film: null
+    }
+  },
+  methods:{
+    getFilm(){
+      axios.get('https://api.themoviedb.org/3/movie/550?api_key=7917990738a6b09dbb79384b066eca6b')
+        .then((film)=>{
+          this.film=film.data;
+        });
     }
   }
 }
