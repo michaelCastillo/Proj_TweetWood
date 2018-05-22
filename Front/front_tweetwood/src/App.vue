2d01af32d8b1cf3ba2abc4bf60b6794490caccf8
Héctor Pérez M <hector.perez.m@usach.cl>
2018-05-21 20:01:59 -0400

    Revert "Falta agregar visualizar la película en particular, los gráficos asociados a la indexación y el módulo de administrador."
    This reverts commit 207625d7e91c2fc0d3147d3fad2f6ad3bbe4340d.

---
+++ Front/front_tweetwood/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <ul>
+      <li><router-link to='/'>Home</router-link></li>
+      <li><router-link to='/graph'>Grafo</router-link></li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Bienvenido a TweetWood'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Front/front_tweetwood/src/Graphic.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'graphic',
+  data () {
+    return {
+      msg: 'Gráfico'
+    }
+  }
+}
+</script>
